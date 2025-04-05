1042b21d277b94e4315217af0b2dfd731c857c5e
henyuer <2940276020@qq.com>
2025-04-05 21:04:29 +0800

    修改原代码，使用异步加载纹理图片，解决了纹理加载不出导致模型一直无法渲染的问题

--- data/src/loads/loadOBJ.js
+++ data/src/loads/loadOBJ.js
@@ -29,14 +29,24 @@ function loadOBJ(renderer, path, name, objMaterial, transform) {
 							else mat = child.material;
 
 							var indices = Array.from({ length: geo.attributes.position.count }, (v, k) => k);
+
 							let mesh = new Mesh({ name: 'aVertexPosition', array: geo.attributes.position.array },
 								{ name: 'aNormalPosition', array: geo.attributes.normal.array },
 								{ name: 'aTextureCoord', array: geo.attributes.uv.array },
 								indices, transform);
 
 							let colorMap = new Texture();
+							// console.log(path + mat.map.name + "1");
 							if (mat.map != null) {
-								colorMap.CreateImageTexture(renderer.gl, mat.map.image);
+								// colorMap.CreateImageTexture(renderer.gl, mat.map.image); //原代码，无法及时加载出image
+								const textureLoader = new THREE.TextureLoader();
+								//异步加载
+								const texture = textureLoader.load(path + mat.name + ".png", () => { // onLoad callback
+									colorMap.CreateImageTexture(renderer.gl, texture.image);
+								}, undefined, (error) => { // onError callback
+									console.error("Error loading texture:", path + mat.map.name, error);
+									colorMap.CreateConstantTexture(renderer.gl, [1, 0, 1]); // 加载失败时使用备用颜色
+								});
 							}
 							else {
 								colorMap.CreateConstantTexture(renderer.gl, mat.color.toArray());
@@ -49,8 +59,8 @@ function loadOBJ(renderer, path, name, objMaterial, transform) {
 							let light = renderer.lights[0].entity;
 							switch (objMaterial) {
 								case 'PhongMaterial':
-									material = buildPhongMaterial(colorMap, mat.specular.toArray(), light, Translation, Scale, "./src/shaders/phongShader/phongVertex.glsl", "./src/shaders/phongShader/phongFragment.glsl");
-									shadowMaterial = buildShadowMaterial(light, Translation, Scale, "./src/shaders/shadowShader/shadowVertex.glsl", "./src/shaders/shadowShader/shadowFragment.glsl");
+									material = buildPhongMaterial(colorMap, mat.specular.toArray(), light, Translation, Scale, "./src/shaders/phongShader/phongVertex.vert", "./src/shaders/phongShader/phongFragment.frag");
+									shadowMaterial = buildShadowMaterial(light, Translation, Scale, "./src/shaders/shadowShader/shadowVertex.vert", "./src/shaders/shadowShader/shadowFragment.frag");
 									break;
 							}
 
