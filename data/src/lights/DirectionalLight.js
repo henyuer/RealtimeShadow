54b1ca0ab101115b3d2ed856a2fdf252529c5281
henyuer <2940276020@qq.com>
2025-04-05 16:06:45 +0800

    初始化，同时实现了lightMVP

---
+++ data/src/lights/DirectionalLight.js
@@ -0,0 +1,45 @@
+class DirectionalLight {
+
+    constructor(lightIntensity, lightColor, lightPos, focalPoint, lightUp, hasShadowMap, gl) {
+        this.mesh = Mesh.cube(setTransform(0, 0, 0, 0.2, 0.2, 0.2, 0));
+        this.mat = new EmissiveMaterial(lightIntensity, lightColor);
+        this.lightPos = lightPos;
+        this.focalPoint = focalPoint;
+        this.lightUp = lightUp
+
+        this.hasShadowMap = hasShadowMap;
+        this.fbo = new FBO(gl);
+        if (!this.fbo) {
+            console.log("无法设置帧缓冲区对象");
+            return;
+        }
+    }
+
+    CalcLightMVP(translate, scale) {
+        let lightMVP = mat4.create();
+        let modelMatrix = mat4.create();
+        let viewMatrix = mat4.create();
+        let projectionMatrix = mat4.create();
+
+        // Model transform
+        mat4.translate(modelMatrix, modelMatrix, translate);
+        mat4.scale(modelMatrix, modelMatrix, scale);
+
+        // View transform
+        mat4.lookAt(viewMatrix, this.lightPos, this.focalPoint, this.lightUp);
+
+        // Projection transform
+        let left = -10;
+        let right = 10;
+        let bottom = -10;
+        let top = 10;
+        let near = 0.1;
+        let far = 100;
+        mat4.ortho(projectionMatrix, left, right, bottom, top, near, far);
+
+        mat4.multiply(lightMVP, projectionMatrix, viewMatrix);
+        mat4.multiply(lightMVP, lightMVP, modelMatrix);
+
+        return lightMVP;
+    }
+}
